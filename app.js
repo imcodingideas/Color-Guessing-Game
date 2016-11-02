2afd728a760930387f0a718afdf4d7f0f7da7910
Joseph Chambers <joseph@michael-chambers.com>
2016-11-02 13:53:24 -0700

    added eventlistener to every square to compare against the pickedColor

--- app.js
+++ app.js
@@ -12,7 +12,24 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll('.square');
+var pickedColor = colors[3];
+var colorDisplay = document.querySelector('#colorDisplay');
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++) {
+    //add initial colors to squares
     squares[i].style.background = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener('click', function () {
+       //grab color of clicked square
+        var clickedColor = this.style.background;
+       //compare color to pickedColor
+        if(clickedColor === pickedColor) {
+            alert('correct');
+        } else {
+            alert('wrong');
+        }
+    });
 }
