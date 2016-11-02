44d0e8825372a774a9c9c42e936815006d85f4e3
Joseph Chambers <joseph@michael-chambers.com>
2016-11-02 13:42:09 -0700

    changed the color of the square divs

--- app.js
+++ app.js
@@ -2,4 +2,17 @@
  * Created by joseph on 11/2/16.
  */
 
-console.log('connected');
+var colors = [
+    "rgb(26, 188, 156)",
+    "rgb(46, 204, 113)",
+    "rgb(231, 76, 60)",
+    "rgb(236, 240, 241)",
+    "rgb(149, 165, 166)",
+    "rgb(241, 196, 15)"
+];
+
+var squares = document.querySelectorAll('.square');
+
+for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+}
